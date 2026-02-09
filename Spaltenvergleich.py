6eb7ed7a82d9aa91feb26b6e0b0d9ed1a24fda97
Immler.Daniel <Daniel.Immler@estw.de>
2026-02-09 15:40:01 +0100

    Verfeinert

--- Spaltenvergleich.py
+++ Spaltenvergleich.py
@@ -7,24 +7,20 @@ import pandas as pd
 
 #file paths
 path_folder = Path(r"\\estw-01\Bereich-N\NG\NGE\Statistiken ESTW\20-kV-Stationen")
-path_Netzleitzahlen = path_folder / "20260202_Netzleitzahlen.xlsm"
-path_GIS_Export = path_folder / "GIS-Export_Station_20260202.xlsx"
-#path_K3_Export = path_folder / "K3-Export_Vergleich k3v zu Netz.xlsx"
+path_Netzleitzahlen = path_folder / "20260209_Netzleitzahlen.xlsm"
+path_GIS_Export = path_folder / "GIS-Export_Station_20260209.xlsx"
 
-#excel einlesen
+#Excel einlesen
 excel_Netzleitzahlen = pd.read_excel(path_Netzleitzahlen, sheet_name="Stationsliste")
 excel_Netzleitzahlen['NLZ'] = pd.to_numeric(excel_Netzleitzahlen['NLZ'], errors='coerce')
 excel_GIS_Export = pd.read_excel(path_GIS_Export, sheet_name="Station", header=4) #header=4: erste vier Zeilen werden ignoriert, Zeile 5 in Spaltenname
 excel_GIS_Export['Stations ID'] = pd.to_numeric(excel_GIS_Export['Stations ID'], errors='coerce')
-#excel_K3_Export = pd.read_excel(path_K3_Export, sheet_name="Tabelle1")
 
 #Datensätze zusammenführen
 merge_nlz: pd.DataFrame = pd.merge(excel_Netzleitzahlen, excel_GIS_Export, left_on='NLZ', right_on='Stations ID', how='outer')
 merge_nlz["Netz"] = merge_nlz["Netz"] + "netz"
-#merge_nlz['NLZ'] = pd.to_numeric(merge_nlz['NLZ'], errors='coerce')
-#merge_nlz['Stations ID'] = pd.to_numeric(merge_nlz['Stations ID'], errors='coerce')
 
-# Unterschiede finden
+#Unterschiede finden
 difference_nkz = merge_nlz[merge_nlz['NKZ'].fillna('') != merge_nlz['Kurzname'].fillna('')]
 only_nkz = difference_nkz[['NKZ', 'Kurzname']]
 difference_nlz = merge_nlz[merge_nlz['NLZ'].fillna('') != merge_nlz['Stations ID'].fillna('')]
@@ -35,9 +31,9 @@ difference_netz =  merge_nlz[merge_nlz['Netz'].notna() & merge_nlz['Teilnetz'].n
 difference_netz['Netz'] = difference_netz['Netz'].str.replace('netz', '', regex=False)  # "netz" aus der Spalte "Netz" entfernen
 only_netz = difference_netz[['Netz', 'Teilnetz', 'NKZ', 'Kurzname']]
 
-# Excel-Datei kreiren
+#Excel-Datei kreieren
 #merge_NKZ.to_excel('Gesamt.xlsx', index=False)
-with pd.ExcelWriter('Unterschiede.xlsx', engine='openpyxl') as writer:
+with pd.ExcelWriter('Unterschiede_GIS_Netzleitzahlen.xlsx', engine='openpyxl') as writer:
     only_nlz.to_excel(writer, sheet_name='NLZ', index=False)
     only_nkz.to_excel(writer, sheet_name='NKZ', index=False)
     only_address.to_excel(writer, sheet_name='Adresse', index=False)
