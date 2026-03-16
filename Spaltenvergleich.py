113f221fddf6778626cefeaf11b8f6b2e608556e
Immler.Daniel <Daniel.Immler@estw.de>
2026-03-16 15:35:44 +0100

    Version mit doppelte Namen/Leistungen anzeigen. Im nächsten Commit heraus.

--- Spaltenvergleich.py
+++ Spaltenvergleich.py
@@ -7,29 +7,36 @@ from openpyxl.formatting.rule import FormulaRule
 import io
 from pathlib import Path
 import matplotlib.pyplot as plt
+from datetime import datetime
 from utils import highlight_differences, de_sort_key
 
 # Spalte einer Excel-Datei filtern nach bestimmten Zeichen
 
 # !!! Überpüfen: !!!
-path_folder = Path(r"C:\Users\immler.daniel\OneDrive - Erlanger Stadtwerke AG\Dokumente\j-PythonTemp")    #Dateipfad überprüfen
-file_1_name = 'Trafoliste_mit_N2.xlsx'
-file_1_sheet_name = 'Tabelle1'
-file_1_header = 2
-file_1_filter_column = 'Nummer NKZ'
-file_1_filter_character = 't'
-file_1_needed_columns = ['Nummer NKZ']
+path_folder = Path(r'C:\Users\immler.daniel\OneDrive - Erlanger Stadtwerke AG\Dokumente\j-PythonTemp\Quellen')    #Dateipfad überprüfen
+file_1_name = 'Auszug Marktstammdatenregister 23.02.2026'
+file_1_sheet_name = 'Stromerzeuger (77)'
+file_1_header = 0
+file_1_needed_columns = ['MaStR-Nr. der Einheit', 'Anzeige-Name der Einheit', 'Betriebsstatus', 'Systemstatus', 'NBP-Status', 'Energieträger', 'Bruttoleistung der Einheit', 'Inbetriebnahmedatum der Einheit', 'Straße']
 
 # Excel einlesen
-excel_file_1 = pd.read_excel(path_folder / file_1_name, file_1_sheet_name, header=file_1_header)
-excel_file_1[['Nummer NLZ', 'Nummer NKZ']] = (excel_file_1['Nummer'].str.extract(r'.*?(\d{3})\s*/\s*(.+)')) # NLZ und NKZ in eigenen Spalten
+excel_file_1 = pd.read_excel(path_folder / (file_1_name + '.xlsx'), file_1_sheet_name, header=file_1_header)
+
+#excel_file_1 = excel_file_1[excel_file_1[file_1_filter_column].astype(str).str.contains(file_1_filter_character, na=False)]
+excel_file_1["Straße"] = excel_file_1["Straße"] + " " + excel_file_1["Hausnummer"].fillna("").astype(str)
 excel_file_1 = excel_file_1[file_1_needed_columns]  # auskommentieren, wenn alle Spalten angezeigt werden sollen
-excel_file_1 = excel_file_1[excel_file_1[file_1_filter_column].astype(str).str.contains(file_1_filter_character, na=False)]
+excel_file_1 = excel_file_1[excel_file_1["Energieträger"] == "Solare Strahlungsenergie"]
+df_multiple = excel_file_1[excel_file_1.duplicated(subset=["Straße", "Bruttoleistung der Einheit"], keep=False)]
+
+df_multiple["_sort_key"] = (df_multiple["Straße"].fillna(df_multiple['Anzeige-Name der Einheit']).astype(str).str.strip().apply(de_sort_key))
+df_multiple = (df_multiple.sort_values(by="_sort_key").drop(columns="_sort_key").reset_index(drop=True))
 
 # Excel-Datei kreiren
-file_name = 'Filtern_' + file_1_name.split('.')[0] + '.xlsx'
-file_path = path_folder / file_name
+file_name = 'Auswertung_MaStR'
+date_today = datetime.now().strftime('%Y%m%d') + '_'
+path_folder = Path(r'C:\Users\immler.daniel\OneDrive - Erlanger Stadtwerke AG\Dokumente\j-PythonTemp\Ergebnisse')
+file_path = path_folder / (date_today + file_name + '.xlsx')
 
-excel_file_1.to_excel(file_path, index=False)
+df_multiple.to_excel(file_path, index=False)
 print("Datei wurde erfolgreich gespeichert!")
 
