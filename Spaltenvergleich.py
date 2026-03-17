cb798fa2b2e5a0cef81e6dd66a3f54c7b3dd738c
Immler.Daniel <Daniel.Immler@estw.de>
2026-03-17 08:56:00 +0100

    K

--- Spaltenvergleich.py
+++ Spaltenvergleich.py
@@ -24,20 +24,20 @@ file_1_needed_columns = ['MaStR-Nr. der Einheit', 'Anzeige-Name der Einheit', 'B
 excel_file_1 = pd.read_excel(path_folder / (file_1_name + '.xlsx'), file_1_sheet_name, header=file_1_header)
 
 #excel_file_1 = excel_file_1[excel_file_1[file_1_filter_column].astype(str).str.contains(file_1_filter_character, na=False)]
-excel_file_1["Straße"] = excel_file_1["Straße"] + " " + excel_file_1["Hausnummer"].fillna("").astype(str)
+excel_file_1['Straße'] = excel_file_1['Straße'] + ' ' + excel_file_1['Hausnummer'].fillna('').astype(str)
 excel_file_1 = excel_file_1[file_1_needed_columns]  # auskommentieren, wenn alle Spalten angezeigt werden sollen
-excel_file_1 = excel_file_1[excel_file_1["Energieträger"] == "Solare Strahlungsenergie"]
-excel_file_1 = excel_file_1[excel_file_1["Betriebsstatus"] == "In Betrieb"]
-excel_file_1 = excel_file_1[excel_file_1["Systemstatus"] == "Aktiviert"]
+excel_file_1 = excel_file_1[excel_file_1['Energieträger'] == 'Solare Strahlungsenergie']
+excel_file_1 = excel_file_1[excel_file_1['Betriebsstatus'] == 'In Betrieb']
+excel_file_1 = excel_file_1[excel_file_1['Systemstatus'] == 'Aktiviert']
 
 # Filtern nach doppelten und diese sortieren
-df_multiple = excel_file_1[excel_file_1.duplicated(subset=["Straße", "Bruttoleistung der Einheit"], keep=False)]
-df_multiple["_sort_key"] = (df_multiple["Straße"].fillna(df_multiple['Anzeige-Name der Einheit']).astype(str).str.strip().apply(de_sort_key))
-df_multiple = (df_multiple.sort_values(by="_sort_key").drop(columns="_sort_key").reset_index(drop=True))
+df_multiple = excel_file_1[excel_file_1.duplicated(subset=['Straße', 'Bruttoleistung der Einheit'], keep=False)]
+df_multiple['_sort_key'] = (df_multiple['Straße'].fillna(df_multiple['Anzeige-Name der Einheit']).astype(str).str.strip().apply(de_sort_key))
+df_multiple = (df_multiple.sort_values(by='_sort_key').drop(columns='_sort_key').reset_index(drop=True))
 
 # DataFrame mit der Leistung nach Jahren erstellen
-excel_file_1["Inbetriebnahmedatum der Einheit"] = excel_file_1["Inbetriebnahmedatum der Einheit"].dt.year
-df_power_per_year = (excel_file_1.groupby("Inbetriebnahmedatum der Einheit")["Bruttoleistung der Einheit"].sum().reset_index())
+excel_file_1['Inbetriebnahmedatum der Einheit'] = excel_file_1['Inbetriebnahmedatum der Einheit'].dt.year
+df_power_per_year = (excel_file_1.groupby('Inbetriebnahmedatum der Einheit')['Bruttoleistung der Einheit'].sum().reset_index())
 
 # Excel-Datei kreiren
 file_name = 'Auswertung_MaStR'
@@ -46,10 +46,10 @@ path_folder = Path(r'C:\Users\immler.daniel\OneDrive - Erlanger Stadtwerke AG\Do
 file_path = path_folder / (date_today + file_name + '.xlsx')
 
 with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
-    excel_file_1.to_excel(writer, sheet_name="Gesamte PVs", index=False)
-    df_power_per_year.to_excel(writer, sheet_name="Leistung_pro_Jahr", index=False)
-    df_multiple.to_excel(writer, sheet_name="Doppelte Anlagen", index=False)
-automatic_column_width(file_path, sheet_name="Gesamte PVs")
-automatic_column_width(file_path, sheet_name="Doppelte Anlagen")
-print("Datei wurde erfolgreich gespeichert!")
+    excel_file_1.to_excel(writer, sheet_name='Gesamte PVs', index=False)
+    df_power_per_year.to_excel(writer, sheet_name='Leistung_pro_Jahr', index=False)
+    df_multiple.to_excel(writer, sheet_name='Doppelte Anlagen', index=False)
+automatic_column_width(file_path, sheet_name='Gesamte PVs')
+automatic_column_width(file_path, sheet_name='Doppelte Anlagen')
+print('Datei wurde erfolgreich gespeichert!')
 
