dc3847106e6dab6876a8ee5c0960d29c2c4103c9
Immler.Daniel <Daniel.Immler@estw.de>
2026-03-23 16:57:45 +0100

    K

--- Spaltenvergleich.py
+++ Spaltenvergleich.py
@@ -7,34 +7,36 @@ from openpyxl.formatting.rule import FormulaRule
 import io
 from pathlib import Path
 import matplotlib.pyplot as plt
-from utils import highlight_differences, de_sort_key
+from utils import highlight_differences, de_sort_key, create_differences_sheet
 
 # Mergen von Zeilen zweier beliebiger Excel-Dateien
 
 # !!! Überpüfen: !!!
-path_folder = Path(r"C:\Users\immler.daniel\OneDrive - Erlanger Stadtwerke AG\Dokumente\j-PythonTemp")    #Dateipfad überprüfen
-file_1_name = 'TS Messungen_20260311'
-file_1_sheet_name = 'Übersicht'
-file_1_merging_column = 'TS'
-file_1_header = 3
-file_1_needed_columns = ['TS']
+path_folder = Path(r"C:\Users\immler.daniel\OneDrive - Erlanger Stadtwerke AG\Dokumente\j-PythonTemp\MaStR")    #Dateipfad überprüfen
+file_1_name = '20260316_Auswertung_MaStR'
+file_1_sheet_name = 'Gesamte PVs'
+file_1_merging_column = 'Straße'
+file_1_header = 0
+file_1_needed_columns = ['Anzeige-Name der Einheit', 'Bruttoleistung der Einheit', 'Nettonennleistung der Einheit', 
+                         'Inbetriebnahmedatum der Einheit', 'Straße']
 
-file_2_name = '2025-04-04_Stationen_Messwerte'
+file_2_name = '20260316_ESTW-EEG-DB'
 file_2_sheet_name = 'Tabelle1'
-file_2_merging_column = 'Anlage'
-file_2_header = 1
-file_2_needed_columns = ['Anlage', 'Abschlussdatum', 'Leistung[kW]', 'Leistung[kVA]']
+file_2_merging_column = 'Strasse der Anlage'
+file_2_header = 0
+file_2_needed_columns = ['Strasse der Anlage', 'PLZ der Anlage', 'Objekt Info', 'LeistungErzeugung']
+
+compare_columns = [('Bruttoleistung der Einheit', 'LeistungErzeugung')]
 
 # Excel einlesen
 excel_file_1 = pd.read_excel(path_folder / (file_1_name +'.xlsx'), sheet_name=file_1_sheet_name, header=file_1_header)
 excel_file_1 = excel_file_1[file_1_needed_columns]
 excel_file_2 = pd.read_excel(path_folder / (file_2_name +'.xlsx'), sheet_name=file_2_sheet_name, header=file_2_header)
+excel_file_2 = excel_file_2[excel_file_2['Art der Erzeugung'].isin(['PV', 'PV-Balkonanlage', 'PV-Freiflächeanlage'])]
 excel_file_2 = excel_file_2[file_2_needed_columns]
 
 # Daten individuel bearbeiten
-excel_file_2['Abschlussdatum'] = pd.to_datetime(excel_file_2['Abschlussdatum'], dayfirst=True)
-excel_file_2 = excel_file_2[excel_file_2['Abschlussdatum'].dt.year == 2024]
-excel_file_2['Leistung[kVA]'] = excel_file_2['Leistung[kVA]'].round(2)
+excel_file_1.rename(columns={'AlterName': 'NeuerName'}, inplace=True)
 
 # Anzahl Spalten bestimmen
 nb_columns_excel_file_1 = excel_file_1.shape[1]
@@ -46,15 +48,15 @@ merged_files: pd.DataFrame = pd.merge(excel_file_1, excel_file_2, left_on= file_
 #merged_files.to_excel('Gesamt.xlsx', index=False)
 
 # Excel-Datei kreiren
-file_name = 'Unterschiede_' + file_1_name.split('.')[0] + '-' + file_2_name.split('.')[0] + '.xlsx'
+file_name = 'Unterschiede_' + file_1_name.split('.')[0] + '.xlsx'
 file_path = path_folder / file_name
+sheet_name='Vergleich'
 with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
-    merged_files.to_excel(writer, sheet_name='Vergleich', index=False, startrow=1)
+    merged_files.to_excel(writer, sheet_name=sheet_name, index=False, startrow=1)
 
     # Zugriff auf das Workbook und Worksheet
     workbook = writer.book
     worksheet = writer.sheets['Vergleich']
-
     # Überschrift erstellen und formatieren
     worksheet.merge_cells(start_row=1, start_column=1, end_row=1, end_column=nb_columns_excel_file_1)
     worksheet.merge_cells(start_row=1, start_column=nb_columns_excel_file_1+1, end_row=1, end_column=nb_columns)
@@ -63,42 +65,67 @@ with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
     cell_left.value = file_1_name.split('.')[0]
     cell_right.value = file_2_name.split('.')[0]
     center_align = Alignment(horizontal="center", vertical="center")
-
     cell_left.fill = PatternFill(start_color="C6EFCE", fill_type="solid")   # grüne Formatierung
     cell_left.font = Font(bold=True, size=16)
     cell_left.alignment = center_align
-
     cell_right.fill = PatternFill(start_color="BDD7EE", fill_type="solid") # blaue Foramtierung
     cell_right.font = Font(bold=True, size=16)
     cell_right.alignment = center_align
-
     for col in range(1, nb_columns + 1):
         cell = worksheet.cell(row=2, column=col)
         cell.fill = PatternFill(start_color="E7E6E6", fill_type="solid")    # graue Formatierung
         cell.font = Font(bold=True)
-
     # Trennungslinie erzeugen
     max_row = worksheet.max_row
     for row in range(1, max_row + 1):
         cell = worksheet.cell(row=row, column=nb_columns_excel_file_1)
         cell.border = Border(right=Side(style='thick'))
-
     # Automatisch Spaltenbreite
     for column in worksheet.columns:
         max_length = 0
         column_letter = get_column_letter(column[0].column)
-
         for cell in column:
             try:
                 if cell.value:
                     max_length = max(max_length, len(str(cell.value)))
             except:
                 pass
-
         adjusted_width = max_length + 2  # etwas Puffer
         if adjusted_width > 30 :    # Maximalbreite setzen
             adjusted_width = 30
         worksheet.column_dimensions[column_letter].width = adjusted_width
+
+header_row = 2  # in dieser Reihe stehen meine Spaltenüberschriften
+
+# Temporäres Sheet erstellen, wird für die Funktion create_differences_sheet benötigt
+workbook = load_workbook(file_path)
+worksheet = workbook[sheet_name]
+worksheet_copy = workbook.copy_worksheet(worksheet)
+worksheet_copy.title ='Temp'
+workbook.save(file_path)
+
+# Unterschiede zwischen den Spalten grafisch hervorheben
+all_rows_different = []
+for idx, compare_column in enumerate(compare_columns):
+    rows_different = highlight_differences(file_path, sheet_name, compare_column[0], compare_column[1], header_row, valid_combinations=None)
+    all_rows_different.append(rows_different)
+
+# # Schnittmenge aller Listen finden
+# common_numbers = set(all_rows_different[0])
+# for row in all_rows_different[1:]:
+#     common_numbers &= set(row)
+# all_rows_different = [[x for x in row if x not in common_numbers] for row in all_rows_different]    # Zahlen aus jeder Liste entfernen
+# all_rows_different.append(list(common_numbers)) # Gemeinsame Zahlen als neue Liste anhängen
+
+# Sheets mit den Unterschieden erstellen
+for idx, compare_column in enumerate(compare_columns):
+    create_differences_sheet(file_path, compare_column, 'Leistung', all_rows_different[idx], header_row)
+# create_differences_sheet(file_path, ('leer', 'leer'), 'leer', all_rows_different[-1], header_row)
+
+# Temporäres Sheet löschen
+workbook = load_workbook(file_path)
+del workbook['Temp']
+workbook.save(file_path)
 
 print("Datei wurde erfolgreich gespeichert!")
 
--- utils.py
+++ utils.py
@@ -4,37 +4,26 @@ from openpyxl.formatting.rule import FormulaRule
 import unicodedata
 
 # Vergleicht die Werte in den angegebenen Spalten und hebt die Unterschiede hervor
-def highlight_differences(file_name, sheet_name, col_left, col_right, header_row, valid_combinations, empty_is_difference):
+def highlight_differences(file_name, sheet_name, col_left, col_right, header_row, valid_combinations):
     wb = load_workbook(file_name)
     ws = wb[sheet_name]
-
     headers = {cell.value: cell.column for cell in ws[header_row]}
+    rows_different = []
 
     for row in range(header_row + 1, ws.max_row + 1):
         c1 = ws.cell(row=row, column=headers[col_left])
         c2 = ws.cell(row=row, column=headers[col_right])
         
-        if not empty_is_difference and (c1.value is None or c2.value is None):
-            #continue
-            one_is_empty = True
-
-        if not valid_combinations:
-            if c1.value != c2.value:
-                if one_is_empty:
-                    c1.fill = PatternFill(start_color='FFEB9C', fill_type='solid')  # gelbe Formatierung
-                    c2.fill = PatternFill(start_color='FFEB9C', fill_type='solid')  # gelbe Formatierung
-                    one_is_empty = False
-                else:
-                    c1.fill = PatternFill(start_color='FFCCCC', fill_type='solid')  # rote Formatierung
-                    c2.fill = PatternFill(start_color='FFCCCC', fill_type='solid')  # rote Formatierung
-        else:
-            if (c1.value, c2.value) in valid_combinations:
-                continue
-
-            c1.fill = PatternFill(start_color='FFCCCC', fill_type='solid')  # rote Formatierung
-            c2.fill = PatternFill(start_color='FFCCCC', fill_type='solid')  # rote Formatierung
+        if c1.value != c2.value:
+            if valid_combinations is not None and (c1.value, c2.value) in valid_combinations:
+                    continue
+            else:
+                c1.fill = PatternFill(start_color='FFCCCC', fill_type='solid')  # rote Formatierung
+                c2.fill = PatternFill(start_color='FFCCCC', fill_type='solid')  # rote Formatierung
+                rows_different.append(row)
 
     wb.save(file_name)
+    return rows_different
 
 
 # Hilfsfunktion zum Sortieren von Strings mit deutschen Umlauten
@@ -45,3 +34,21 @@ def de_sort_key(s):
         .decode("ascii")
         .lower()
     )
+
+def create_differences_sheet(file_name, compare_column, sheet_title, rows_different, header_row):
+    wb = load_workbook(file_name)
+    ws = wb['Temp']
+    headers = {cell.value: cell.column for cell in ws[header_row]}
+    worksheet_copy = wb.copy_worksheet(ws)
+    worksheet_copy.title = sheet_title
+
+    for row in range(worksheet_copy.max_row, header_row, -1):    # zählt rückwärts und stoppt bei header_row
+        if row not in rows_different:
+             worksheet_copy.delete_rows(row)
+        else:
+            if compare_column == ('leer', 'leer'):
+                 continue
+            worksheet_copy.cell(row=row, column=headers[compare_column[0]]).fill = PatternFill(start_color='FFCCCC', fill_type='solid')  # rote Formatierung
+            worksheet_copy.cell(row=row, column=headers[compare_column[1]]).fill = PatternFill(start_color='FFCCCC', fill_type='solid')  # rote Formatierung
+
+    wb.save(file_name)
