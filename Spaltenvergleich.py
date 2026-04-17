fff5c426d893c9815bc518db0823a0a859f69875
Immler.Daniel <Daniel.Immler@estw.de>
2026-04-17 16:52:00 +0200

    Große Auswertung hinzugefügt

--- Spaltenvergleich.py
+++ Spaltenvergleich.py
@@ -18,37 +18,76 @@ file_1_sheet_name = 'Stromerzeuger (81)'
 file_1_header = 0
 file_1_needed_columns = ['MaStR-Nr. der Einheit', 'Anzeige-Name der Einheit', 'Betriebsstatus', 'Systemstatus', 'NBP-Status', 'Energieträger', 'Bruttoleistung der Einheit', 
                          'Nettonennleistung der Einheit', 'Inbetriebnahmedatum der Einheit', 'Registrierungsdatum der Einheit',
-                         'Straße']
+                         'Postleitzahl', 'Straße']
 
 # Excel einlesen
 excel_file_1 = pd.read_excel(path_folder / (file_1_name + '.xlsx'), file_1_sheet_name, header=file_1_header)
 
-#excel_file_1 = excel_file_1[excel_file_1[file_1_filter_column].astype(str).str.contains(file_1_filter_character, na=False)]
+# Daten bearbeiten
 excel_file_1['Straße'] = excel_file_1['Straße'] + ' ' + excel_file_1['Hausnummer'].fillna('').astype(str)
 excel_file_1 = excel_file_1[file_1_needed_columns]  # auskommentieren, wenn alle Spalten angezeigt werden sollen
-excel_file_1 = excel_file_1[excel_file_1['Energieträger'] == 'Solare Strahlungsenergie']
 excel_file_1 = excel_file_1[excel_file_1['Betriebsstatus'] == 'In Betrieb']
 excel_file_1 = excel_file_1[excel_file_1['Systemstatus'] == 'Aktiviert']
-
-# Filtern nach doppelten und diese sortieren
-df_multiple = excel_file_1[excel_file_1.duplicated(subset=['Straße', 'Bruttoleistung der Einheit'], keep=False)]
-df_multiple['_sort_key'] = (df_multiple['Straße'].fillna(df_multiple['Anzeige-Name der Einheit']).astype(str).str.strip().apply(de_sort_key))
-df_multiple = (df_multiple.sort_values(by='_sort_key').drop(columns='_sort_key').reset_index(drop=True))
-
-# DataFrame mit der Leistung nach Jahren erstellen
 excel_file_1['Inbetriebnahmedatum der Einheit'] = excel_file_1['Inbetriebnahmedatum der Einheit'].dt.year
-df_power_per_year = (excel_file_1.groupby('Inbetriebnahmedatum der Einheit')['Bruttoleistung der Einheit'].sum().reset_index())
+
+df_all_pv = excel_file_1[excel_file_1['Energieträger'] == 'Solare Strahlungsenergie']
+df_all_storage = excel_file_1[excel_file_1['Energieträger'] == 'Speicher']
+
+# Dataframe gesamt installierte PV nach Jahren
+df_all_pv_power_per_year = (df_all_pv.groupby('Inbetriebnahmedatum der Einheit')['Bruttoleistung der Einheit'].sum().reset_index())
+df_all_pv_power_per_year = df_all_pv_power_per_year.rename(columns={'Bruttoleistung der Einheit': 'Zuwachs pro Jahr', 'Inbetriebnahmedatum der Einheit': 'Jahr'})
+df_all_pv_power_per_year['Summe'] = (df_all_pv_power_per_year['Zuwachs pro Jahr'].cumsum())
+
+# DataFrame gesamt PV nach Größe
+sum_smaller_25 = df_all_pv.loc[df_all_pv['Bruttoleistung der Einheit'] < 25, 'Bruttoleistung der Einheit'].sum()
+sum_25_until_100 = df_all_pv.loc[(df_all_pv['Bruttoleistung der Einheit'] >= 25) & (df_all_pv['Bruttoleistung der Einheit'] <= 100), 'Bruttoleistung der Einheit'].sum()
+sum_bigger_100 = df_all_pv.loc[df_all_pv['Bruttoleistung der Einheit'] > 100, 'Bruttoleistung der Einheit'].sum()
+df_all_pv_power_per_cap = pd.DataFrame({'< 25 kW': [sum_smaller_25], '25 - 100 kW': [sum_25_until_100], '> 100 kW': [sum_bigger_100]})
+
+# DataFrame Westnetz installierte PV nach Jahren
+df_all_pv_west = df_all_pv[df_all_pv['Postleitzahl'] == 91056]
+df_wn_pv_power_per_year = (df_all_pv_west.groupby('Inbetriebnahmedatum der Einheit')['Bruttoleistung der Einheit'].sum().reset_index())
+df_wn_pv_power_per_year = df_wn_pv_power_per_year.rename(columns={'Bruttoleistung der Einheit': 'Zuwachs pro Jahr', 'Inbetriebnahmedatum der Einheit': 'Jahr'})
+df_wn_pv_power_per_year['Summe'] = (df_wn_pv_power_per_year['Zuwachs pro Jahr'].cumsum())
+
+# DataFrame Westnetz PV nach Größe
+wn_sum_smaller_25 = df_all_pv_west.loc[df_all_pv_west['Bruttoleistung der Einheit'] < 25, 'Bruttoleistung der Einheit'].sum()
+wn_sum_25_until_100 = df_all_pv_west.loc[(df_all_pv_west['Bruttoleistung der Einheit'] >= 25) & (df_all_pv_west['Bruttoleistung der Einheit'] <= 100), 'Bruttoleistung der Einheit'].sum()
+wn_bigger_100 = df_all_pv_west.loc[df_all_pv_west['Bruttoleistung der Einheit'] > 100, 'Bruttoleistung der Einheit'].sum()
+df_wn_pv_power_per_cap = pd.DataFrame({'< 25 kW': [wn_sum_smaller_25], '25 - 100 kW': [wn_sum_25_until_100], '> 100 kW': [wn_bigger_100]})
+
+# DataFrame Speicher
+df_all_storage_per_year = (df_all_storage.groupby('Inbetriebnahmedatum der Einheit')['Bruttoleistung der Einheit'].sum().reset_index())
+df_all_storage_per_year = df_all_storage_per_year.rename(columns={'Bruttoleistung der Einheit': 'Zuwachs pro Jahr', 'Inbetriebnahmedatum der Einheit': 'Jahr'})
+df_all_storage_per_year['Summe'] = (df_all_storage_per_year['Zuwachs pro Jahr'].cumsum())
+
+# DataFrame Speicher nach Größe
+df_all_storage['Bruttoleistung der Einheit gerundet'] = df_all_storage['Bruttoleistung der Einheit'].round(0).astype(int)
+max_storage_capacity = df_all_storage['Bruttoleistung der Einheit gerundet'].max()
+data = []
+for i in range(max_storage_capacity + 1):
+    sum = df_all_storage.loc[df_all_storage['Bruttoleistung der Einheit gerundet'] == i, 'Bruttoleistung der Einheit'].sum()
+    data.append(sum)
+df_all_storage_per_cap = pd.DataFrame({'Kapazität in kW': list(range(max_storage_capacity + 1)), 'Summe': data})
 
 # Excel-Datei kreiren
-file_name = 'Auswertung_MaStR_2024'
+file_name = 'PV_Auswertung_MaStR'
 date_today = datetime.now().strftime('%Y%m%d') + '_'
 file_path = path_folder / (date_today + file_name + '.xlsx')
 
 with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
-    excel_file_1.to_excel(writer, sheet_name='Gesamte PVs', index=False)
-    df_power_per_year.to_excel(writer, sheet_name='Leistung_pro_Jahr', index=False)
-    df_multiple.to_excel(writer, sheet_name='Doppelte Anlagen', index=False)
-automatic_column_width(file_path, sheet_name='Gesamte PVs')
-automatic_column_width(file_path, sheet_name='Doppelte Anlagen')
-print('Datei wurde erfolgreich gespeichert!')
+    df_all_pv.to_excel(writer, sheet_name='Gesamte PVs', index=False)
+    df_all_pv_power_per_year.to_excel(writer, sheet_name='PV-Leistung pro Jahr', index=False)
+    df_all_pv_power_per_cap.to_excel(writer, sheet_name='PV Größenverteilung', index=False)
+    df_wn_pv_power_per_year.to_excel(writer, sheet_name='Westnetz PV-Leistung pro Jahr', index=False)
+    df_wn_pv_power_per_cap.to_excel(writer, sheet_name='Westnetz PV Größenverteilung', index=False)
+    df_all_storage_per_year.to_excel(writer, sheet_name='Speicherleistung pro Jahr', index=False)
+    df_all_storage_per_cap.to_excel(writer, sheet_name='Speicher Größenverteilung', index=False)
+    df_all_storage.to_excel(writer, sheet_name='Gesamte Speicher', index=False)
+
+sheet_names = ['Gesamte PVs', 'PV-Leistung pro Jahr', 'PV Größenverteilung', 'Westnetz PV-Leistung pro Jahr', 'Westnetz PV Größenverteilung', 'Speicherleistung pro Jahr', 
+               'Speicher Größenverteilung', 'Gesamte Speicher']
+for sheet_name in sheet_names: automatic_column_width(file_path, sheet_name=sheet_name)
+
+print('Datei wurde erfolgreich erstellt!')
 
