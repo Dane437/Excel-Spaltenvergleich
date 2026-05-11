c26f5437afae25100d225513c27ff4bf88dd0f00
Immler.Daniel <Daniel.Immler@estw.de>
2026-05-11 14:38:41 +0200

    Ä

--- Spaltenvergleich.py
+++ Spaltenvergleich.py
@@ -19,7 +19,7 @@ file_1_sheet_name = 'Stromerzeuger'
 file_1_header = 0
 file_1_needed_columns = ['MaStR-Nr. der Einheit', 'Anzeige-Name der Einheit', 'Betriebsstatus', 'Systemstatus', 'NBP-Status', 'Energieträger', 'Bruttoleistung der Einheit', 
                          'Nettonennleistung der Einheit', 'Inbetriebnahmedatum der Einheit', 'Registrierungsdatum der Einheit',
-                         'Postleitzahl', 'Straße', 'Nutzbare Speicherkapazität in kWh']
+                         'Postleitzahl', 'Straße', 'Hausnummer', 'Nutzbare Speicherkapazität in kWh']
 
 file_2_name = 'Straßen Westnetz'
 file_2_sheet_name = 'List1'
@@ -50,18 +50,22 @@ df_all_pv_west = df_all_pv[df_all_pv['Postleitzahl'] == 91056]
 merged_df: pd.DataFrame = pd.merge(df_all_pv_west, excel_file_2, on='Straße', how='left')
 
 # 
-list_districts = merged_df.loc[merged_df['Ortsteil'] != '-', 'Ortsteil'].dropna().unique().tolist() # Liste aller Ortsteile
+#list_districts = merged_df.loc[merged_df['Ortsteil'] != '-', 'Ortsteil'].dropna().unique().tolist() # Liste aller Ortsteile
 
 # Excel-Datei kreiren
 file_name = 'Auswertung_MaStR_Westnetz'
 date_today = datetime.now().strftime('%Y%m%d') + '_'
 if run_in_vs_code:
     file_path = path_folder / (date_today + file_name + '.xlsx')
-    file_path_2 = path_folder / (date_today + 'alOrTe.xlsx')
+    file_path_2 = path_folder / 'Zwischenauswertung.xlsx'
 else:
     file_path = date_today + file_name + '.xlsx'
 
-merged_df.to_excel(file_path_2, index=False)
+#merged_df.to_excel(file_path_2, index=False)
+
+merged_df = pd.read_excel(file_path_2, sheet_name='Sheet1')
+list_districts = merged_df.loc[merged_df['Ortsteil'] != '-', 'Ortsteil'].dropna().unique().tolist() # Liste aller Ortsteile
+
 with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
     #df_all_pv.to_excel(writer, sheet_name='Gesamte PVs', index=False)
     for district in list_districts:
